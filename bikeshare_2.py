bacc4805b2f75c7ed8e8888e661dfb69a1e73350
Lea Dingler <lea.dingler@gmail.com>
2025-09-22 22:48:06 +0200

    adding closing words for user

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -221,6 +221,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print('That\'s okay, see you next time you wanna explore some more bikesharing stats.')
             break
 
 
